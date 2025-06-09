122aa3a290aee0e5021e594b81dff55354977183
hyebin <khv2644511@naver.com>
2025-06-09 23:01:56 +0900

    유저조회 api

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,14 +2,19 @@ package com.group.libraryapp.controller.user;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.libraryapp.domain.user.Fruit;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class UserController {
@@ -21,8 +26,21 @@ public class UserController {
         // 사용자 정보를 저장하는 로직을 여기에 작성합니다.
         // 예: userService.save(userCreateRequest);
 
+
         users.add(new User(request.getName() , request.getAge()));
     
 
     }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        List<UserResponse> responses = new ArrayList<>();
+       for (int i =0; i< users.size(); i++) {
+           
+            responses.add(new UserResponse(i + 1, users.get(i)));
+        }
+        return responses;
+    }
+   
+    
 }
